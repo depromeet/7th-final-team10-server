69b7c8576696e43a5bc80a9a79662a3187a792c8
Jaeyeonling <jaeyeonling@gmail.com>
2020-01-07 00:35:37 +0900

    feat: JPA 설정

---
+++ src/main/java/com/depromeet/boiledegg/common/infrastructure/persistence/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.depromeet.boiledegg.common.infrastructure.persistence;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig {
+}

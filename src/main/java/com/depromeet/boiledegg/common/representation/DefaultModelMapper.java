5ea6195ce8a03b143455b917707d28e5f7a91488
Jaeyeonling <jaeyeonling@gmail.com>
2020-01-07 00:36:03 +0900

    feat: 기본 DefaultModelMapper 추가

---
+++ src/main/java/com/depromeet/boiledegg/common/representation/DefaultModelMapper.java
@@ -0,0 +1,8 @@
+package com.depromeet.boiledegg.common.representation;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+final class DefaultModelMapper extends ModelMapper {
+}

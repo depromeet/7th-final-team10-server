7dea88c43c4edf8c8041d0def5ae5eaba7d349c8
Jaeyeonling <jaeyeonling@gmail.com>
2020-01-10 00:20:35 +0900

    feat: Aspect 설정 추가

---
+++ src/main/java/com/depromeet/boiledegg/common/infrastructure/aspect/AspectConfig.java
@@ -0,0 +1,9 @@
+package com.depromeet.boiledegg.common.infrastructure.aspect;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectConfig {
+}

3601c2a4d10be8b36299ea3e407fb019f218c879
Jaeyeonling <jaeyeonling@gmail.com>
2020-02-19 23:50:18 +0900

    feat: 책 관련 추가

--- src/test/java/com/depromeet/boiledegg/book/representation/representation/MockBookSaveRequestFactory.java
+++ src/test/java/com/depromeet/boiledegg/book/representation/representation/MockBookSaveRequestFactory.java
@@ -1,7 +1,10 @@
 package com.depromeet.boiledegg.book.representation.representation;
 
+import com.depromeet.boiledegg.book.domain.BookCategory;
 import com.depromeet.boiledegg.book.representation.BookSaveRequest;
 import lombok.experimental.UtilityClass;
+
+import java.util.Set;
 
 @UtilityClass
 public class MockBookSaveRequestFactory {
@@ -9,6 +12,7 @@ public class MockBookSaveRequestFactory {
     public BookSaveRequest create() {
         return BookSaveRequest.builder()
                 .isbn("9788964150290")
+                .bookCategories(Set.of(BookCategory.values()))
                 .build();
     }
 }

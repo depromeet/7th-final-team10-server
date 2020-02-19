35588f2d8df998ad80d70ccd1894d695465cad30
Jaeyeonling <jaeyeonling@gmail.com>
2020-02-19 23:40:58 +0900

    feat: TODO 추가

--- src/main/java/com/depromeet/boiledegg/EntryPoint.java
+++ src/main/java/com/depromeet/boiledegg/EntryPoint.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EntryPoint {
 
+    // TODO 빌린 것에 대한 상태 추가
+    // TODO 반납일자 다가오면 알람
+
     public static void main(final String... args) {
         SpringApplication.run(EntryPoint.class, args);
     }
